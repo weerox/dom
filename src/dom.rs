5ad28180974d95585bc4500dc7bc87bab0128d33
Aron Bergman <bathingrad@gmail.com>
2022-02-26 00:20:01 +0100

    Create new Dom from &T and increase count

--- src/dom.rs
+++ src/dom.rs
@@ -1,5 +1,6 @@
 use std::cell::Cell;
 use std::ptr::NonNull;
+use std::alloc::Layout;
 
 struct DomMeta<T> {
     count: Cell<usize>,
@@ -44,6 +45,34 @@ impl<T> Clone for Dom<T> {
         }
 }
 
+impl<T> From<&T> for Dom<T> {
+    fn from(reference: &T) -> Dom<T> {
+        let layout = unsafe {
+            Layout::from_size_align_unchecked(
+                Layout::new::<DomMeta<()>>().size(),
+                std::mem::align_of_val(reference)
+            ).pad_to_align()
+        };
+
+        let byte_offset = layout.size() as isize;
+
+        // SAFETY This is safe as long as the incoming &T came from an existing Dom<T>.
+        let ptr: *mut DomMeta<T> = unsafe {
+            (reference as *const T as *mut u8).offset(-byte_offset) as *mut DomMeta<T>
+        };
+
+        let dom = Dom {
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+        };
+
+        let mut count = dom.meta().count.get();
+        count += 1;
+        dom.meta().count.set(count);
+
+        dom
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,5 +84,14 @@ mod tests {
 
         assert_eq!(dom1.meta().count.get(), dom2.meta().count.get());
         assert_eq!(dom1.meta().count.get(), 2);
+    }
+
+    #[test]
+    fn from_ref_increases_count() {
+        let dom = Dom::new(1_u32);
+        let r = &dom.meta().value;
+        let dom_from = Dom::from(r);
+        assert_eq!(dom.meta().count.get(), dom_from.meta().count.get());
+        assert_eq!(dom.meta().count.get(), 2);
     }
 }
