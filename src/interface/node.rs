d668b885126cd9fcd309023469f0ef7d814e4ec5
Aron Bergman <aron@aronbergman.se>
2022-04-21 16:22:45 +0200

    Add basic getter functions to the Node interface

--- src/interface/node.rs
+++ src/interface/node.rs
@@ -16,3 +16,36 @@ impl Interface for Node {
         InterfaceID::new(1)
     }
 }
+
+impl Node {
+    pub fn new() -> Self {
+        Node {
+            _top: unsafe { InterfaceID::zero() },
+            parent: None,
+            first_child: None,
+            last_child: None,
+            previous_sibling: None,
+            next_sibling: None,
+        }
+    }
+
+    pub fn parent(&self) -> Option<Dom<Node>> {
+        self.parent.clone()
+    }
+
+    pub fn first_child(&self) -> Option<Dom<Node>> {
+        self.first_child.clone()
+    }
+
+    pub fn last_child(&self) -> Option<Dom<Node>> {
+        self.last_child.clone()
+    }
+
+    pub fn previous_sibling(&self) -> Option<Dom<Node>> {
+        self.previous_sibling.clone()
+    }
+
+    pub fn next_sibling(&self) -> Option<Dom<Node>> {
+        self.next_sibling.clone()
+    }
+}
