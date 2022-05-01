31770d8ef7ae78cef99d7d189cc24422c821a7f1
Aron Bergman <aron@aronbergman.se>
2022-05-01 12:23:59 +0200

    Implement Deref[Mut] for interfaces with inheritance

--- src/interface/document.rs
+++ src/interface/document.rs
@@ -1,6 +1,8 @@
 use crate::interface::Node;
 use crate::{Interface, InterfaceID};
 
+use std::ops::{Deref, DerefMut};
+
 #[repr(C)]
 pub struct Document {
     _inherited: Node,
@@ -9,5 +11,19 @@ pub struct Document {
 impl Interface for Document {
     fn id() -> InterfaceID {
         InterfaceID::new(2)
+    }
+}
+
+impl Deref for Document {
+    type Target = Node;
+
+    fn deref(&self) -> &Self::Target {
+        &self._inherited
+    }
+}
+
+impl DerefMut for Document {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self._inherited
     }
 }
--- src/interface/element.rs
+++ src/interface/element.rs
@@ -1,6 +1,8 @@
 use crate::interface::Node;
 use crate::{Interface, InterfaceID};
 
+use std::ops::{Deref, DerefMut};
+
 #[repr(C)]
 pub struct Element {
     _inherited: Node,
@@ -9,5 +11,19 @@ pub struct Element {
 impl Interface for Element {
     fn id() -> InterfaceID {
         InterfaceID::new(3)
+    }
+}
+
+impl Deref for Element {
+    type Target = Node;
+
+    fn deref(&self) -> &Self::Target {
+        &self._inherited
+    }
+}
+
+impl DerefMut for Element {
+    fn deref_mut(&mut self)  -> &mut Self::Target {
+        &mut self._inherited
     }
 }
